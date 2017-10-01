2dfaaeb3d444061067d74abc99648866e244a40c
adaschevici <a.daschevici@gmail.com>
2017-10-01 19:15:32 +0100

    Add required packages

--- react-front/src/index.js
+++ react-front/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRedirect, browserHistory } from 'react-router';
+import loggerMiddleware from 'redux-logger';
+import sagaMiddleware from 'redux-saga';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
