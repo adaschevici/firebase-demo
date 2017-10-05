37f16c9612ca2c7c66f8c6dbe3697577f16169dd
adaschevici <a.daschevici@gmail.com>
2017-10-05 22:22:55 +0100

    Add hub

---
+++ react-front/src/components/Hub/Hub.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Hub.css';
+
+import Clap from '../../components/Clap/Clap';
+
+class Hub extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Clap />
+      </div>
+    );
+  }
+}
+
+export default Hub;
+
