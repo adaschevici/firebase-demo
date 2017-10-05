2c5fd741e8f37733c710534c7cdff5a425beef09
adaschevici <a.daschevici@gmail.com>
2017-10-05 23:21:15 +0100

    Update style for mobile

--- react-front/src/routes/index.js
+++ react-front/src/routes/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import Atendee from '../components/Atendee/Atendee';
-// import Hub from '../components/Hub/Hub';
+import Hub from '../components/Hub/Hub';
 
       {/* <Route path="/atendee" component={Clap} />
-      <Route path="/hub" component={Hub} />,
       <Route path="/hub" component={Hub} /> */}
 export default ([
       <Route path="/atendee" component={Atendee} />,
+      <Route path="/hub" component={Hub} />,
 ]);
 
