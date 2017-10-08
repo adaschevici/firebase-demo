8c3e2b438c2813e3e5f732f54aa1e5d39533d79a
adaschevici <a.daschevici@gmail.com>
2017-10-08 18:48:50 +0100

    Add cors headers config

--- python-back/server.py
+++ python-back/server.py
@@ -1,12 +1,28 @@
+import uuid
+import logging
+import logging.handlers
 from aiohttp import web
+import aiohttp_cors
 
-async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+async def user_joined(request):
+    user_id = str(uuid.uuid4())
+    print("User with id %s just joined" % (user_id))
+    uid = { "user_uuid": user_id }
+    return web.json_response(uid)
+
 
 app = web.Application()
-app.router.add_get('/', handle)
-app.router.add_get('/{name}', handle)
+app.router.add_get('/join', user_joined)
+
+cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+})
+
+for route in list(app.router.routes()):
+    cors.add(route)
 
 web.run_app(app)
