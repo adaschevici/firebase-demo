df47986807a8b84e9a0b300efee27f4815f89cf1
adaschevici <a.daschevici@gmail.com>
2017-10-09 19:45:25 +0100

    Update backend to support the definitions of constants

---
+++ python-back/firebase.py
@@ -0,0 +1,41 @@
+import pyrebase
+import constants
+
+config = {
+    "apiKey": constants.API_KEY,
+    "authDomain": constants.AUTH_DOMAIN,
+    "databaseURL": constants.DB_URL,
+    "projectId": constants.PROJECT_ID,
+    "storageBucket": constants.STORAGE_BUCKET,
+    "messagingSenderId": constants.SENDER_ID
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+user = {}
+
+def firebase_insert_user(user_id):
+
+    # Log the user in
+    global user
+    if not user.get('idToken'):
+        print("Logging in")
+        user = auth.sign_in_with_email_and_password(constants.USER_EMAIL, constants.USER_PASSWORD)
+
+    # Get a reference to the database service
+    db = firebase.database()
+
+    # data to save
+    data = {
+        "claps": 0
+    }
+
+    # Pass the user's idToken to the push method
+    results = db.child("users").child(user_id).set(data, user['idToken'])
+
+def firebase_update_claps(user_id):
+    global user
+    if not user.get('idToken'):
+        user = auth.sign_in_with_email_and_password(constants.USER_EMAIL, constants.USER_PASSWORD)
+    db = firebase.database()
+    pass
--- python-back/server.py
+++ python-back/server.py
@@ -2,14 +2,20 @@ import uuid
 import logging
 import logging.handlers
 from aiohttp import web
+import firebase
 import aiohttp_cors
 
 async def user_joined(request):
     user_id = str(uuid.uuid4())
     print("User with id %s just joined" % (user_id))
+    firebase.firebase_insert_user(user_id)
     uid = { "user_uuid": user_id }
     return web.json_response(uid)
 
+async def user_clapped(request):
+    print("User with id %s just clapped" % (user_id))
+    firebase.update_claps(user_id)
+    return web.json_response({ 200: "OK" })
 
 app = web.Application()
 app.router.add_get('/join', user_joined)
