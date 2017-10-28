8b2c0f752270f49edb13d38864e601ce9540293d
adaschevici <a.daschevici@gmail.com>
2017-10-28 21:40:31 +0100

    Add uuid for keys

--- react-front/src/routes/index.js
+++ react-front/src/routes/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import uuid from 'uuid/v4';
 
 import Attendee from '../components/Attendee/Attendee.container';
 import Hub from '../components/Hub/Hub.container';
 
 export default ([
-  <Route path="/attendee" component={Attendee} />,
-  <Route path="/hub" component={Hub} />,
+  <Route path="/attendee" component={Attendee} key={uuid()} />,
+  <Route path="/hub" component={Hub} key={uuid()} />,
 ]);
 
