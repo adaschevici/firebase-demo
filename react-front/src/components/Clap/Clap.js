d0f86e1dbc0b3e023604328a76c6f5839a40e8fc
adaschevici <a.daschevici@gmail.com>
2017-11-16 20:30:11 +0000

    Fix linting

--- react-front/src/components/Clap/Clap.js
+++ react-front/src/components/Clap/Clap.js
@@ -3,12 +3,29 @@ import PropTypes from 'prop-types';
 import { MediumClapTraced, MediumClapFilled } from '../Icons/MediumClap';
 import './Clap.css';
 
-const Clap = props => (props.filled ?
-  <MediumClapFilled width="150" height="150" /> :
-  <MediumClapTraced width="150" height="150" />);
+// const Clap = props => (props.filled ?
+//   <MediumClapFilled width="150" height="150" /> :
+//   <MediumClapTraced width="150" height="150" />);
+
+
+class Clap extends Component {
+  // componentWillReceiveProps(nextProps) {
+  //   console.log(nextProps);
+  // }
+
+  render() {
+    return this.props.filled ?
+      <MediumClapFilled width="150" height="150" /> :
+      <MediumClapTraced width="150" height="150" />;
+  }
+}
+
+Clap.defaultProps = {
+  filled: false,
+};
 
 Clap.propTypes = {
-  filled: PropTypes.bool.isRequired,
+  filled: PropTypes.bool,
 };
 
 export default Clap;
